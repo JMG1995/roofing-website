a62b22f8d7302d7b2949e7d2d34df20231202c36
Joseph <jmgrant1995@gmail.com>
2020-05-02 15:03:50 +0800

    Actually just remove bulma and using plain css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,7 +57,9 @@ const Navbar = class extends React.Component {
 						id="navMenu"
 						className={`navbar-menu ${this.state.navBarActiveClass}`}>
 						<div className="navbar-start has-text-centered">
-							<Link className="navbar-item" to="/about">
+							<Link
+								className="navbar-item text-space-cadet text-2xl"
+								to="/about">
 								About
 							</Link>
 							<Link className="navbar-item" to="/products">
